cf2850836d38718a8718b444c761adc8ecee2d8d
howdidigetthisname@gmail.com <howdidigetthisname@gmail.com@994ab720-7c6f-f82b-cc57-6f511588b643>
2011-12-08 18:46:00 +0000

    Updated JUnit

--- CS2340FinalProject/src/org/jamieandtheboys/tests/JUnit_wchen.java
+++ CS2340FinalProject/src/org/jamieandtheboys/tests/JUnit_wchen.java
@@ -2,6 +2,10 @@ package org.jamieandtheboys.tests;
 
 import static org.junit.Assert.*;
 
+import java.awt.TextArea;
+
+import javax.swing.JProgressBar;
+
 import org.jamieandtheboys.diseases.*;
 import org.jamieandtheboys.persons.Breeder;
 import org.jamieandtheboys.persons.GenericPerson;
@@ -19,14 +23,28 @@ public class JUnit_wchen {
 		GameLogic gl = new GameLogic();
 		gl.run();
 		
+		gl.frame.progressBar_1 = new JProgressBar();
+		gl.frame.progressBar_2 = new JProgressBar();
+		gl.frame.progressBar_3 = new JProgressBar();
+		gl.frame.progressBar_4 = new JProgressBar();
+		gl.frame.progressBar_5 = new JProgressBar();
+		gl.frame.textArea = new TextArea();
+		
 		
 		Breeder guy = new Breeder("Guy");
 		GenericPerson gal = new GenericPerson("Gal");
+		GenericPerson boy = new GenericPerson("Boy");
+		GenericPerson girl = new GenericPerson("Girl");
+		GenericPerson hermaphrodite = new GenericPerson("hermaphrodite");
+		
 		gl.party.add(guy);
 		gl.party.add(gal);	
+		gl.party.add(boy);	
+		gl.party.add(girl);	
+		gl.party.add(hermaphrodite);	
 		
 		gl.pace = 15; //Grueling
-		gl.rations = 2*2; //meager
+		gl.rations = 2*5; //meager
 		
 		assertEquals(guy.getHealth(), 100);
 		assertEquals(gal.getHealth(), 100);
@@ -35,8 +53,8 @@ public class JUnit_wchen {
 		
 		assertEquals(guy.getHealth(), 99);
 		assertEquals(guy.getFatigue(), 17);
-		assertEquals(gal.getHealth(), 99);
-		assertEquals(gal.getFatigue(), 17);
+		assertEquals(boy.getHealth(), 99);
+		assertEquals(boy.getFatigue(), 17);
 		
 		
 		gl.pace = 10; //Normal
@@ -45,20 +63,20 @@ public class JUnit_wchen {
 		
 		assertEquals(guy.getHealth(), 99);
 		assertEquals(guy.getFatigue(), 32);
-		assertEquals(gal.getHealth(), 99);
-		assertEquals(gal.getFatigue(), 32);
+		assertEquals(girl.getHealth(), 99);
+		assertEquals(girl.getFatigue(), 32);
 		
 		gl.pace = 5;   //Leisurely
-		gl.rations = 1*2; //bare-bones
+		gl.rations = 1*5; //bare-bones
 		gl.updateHealth();
 		
 		assertEquals(guy.getHealth(), 100);
 		assertEquals(guy.getFatigue(), 32);
-		assertEquals(gal.getHealth(), 100);
-		assertEquals(gal.getFatigue(), 32);
+		assertEquals(hermaphrodite.getHealth(), 100);
+		assertEquals(hermaphrodite.getFatigue(), 32);
 		
 		gl.pace = 0; //Stopped
-		gl.rations = 4*2; //Well-fed
+		gl.rations = 4*5; //Well-fed
 		gl.updateHealth();
 		
 		assertEquals(guy.getHealth(), 100);
@@ -67,7 +85,7 @@ public class JUnit_wchen {
 		assertEquals(gal.getFatigue(), 17);
 		
 		gl.pace = 15;	//Grueling
-		gl.rations = 3*2; // Normal
+		gl.rations = 3*5; // Normal
 		gl.updateHealth();
 		
 		assertEquals(guy.getHealth(), 99);
@@ -92,15 +110,14 @@ public class JUnit_wchen {
 		
 		//Check player death
 		
-		guy.setHealth(2);
+		guy.setHealth(1);
 		gal.setHealth(1);
+		boy.setHealth(1);
+		girl.setHealth(1);
+		hermaphrodite.setHealth(1);
+
 		gl.updateHealth();
 		
-		assertEquals(gl.party.size(), 1);
-		
-		gl.updateHealth();
-		
-		assertEquals(gl.party.size(), 0);
 		assertEquals(gl.gameover, true);
 	}
 	
