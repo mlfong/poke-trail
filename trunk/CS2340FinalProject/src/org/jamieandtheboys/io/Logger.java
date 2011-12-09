9f3412a5c7cdd9293da578fefd70d7bd4fe5e380
ericwehmueller <ericwehmueller@994ab720-7c6f-f82b-cc57-6f511588b643>
2011-12-09 02:59:24 +0000

    Updated the logging so that it does not include gibberish. Cleaned it up.

--- trunk/CS2340FinalProject/src/org/jamieandtheboys/io/Logger.java
+++ trunk/CS2340FinalProject/src/org/jamieandtheboys/io/Logger.java
@@ -1,47 +1,42 @@
 package org.jamieandtheboys.io;
+
 import java.io.*;
 import java.util.*;
 import org.jamieandtheboys.persons.Person;
+
 public class Logger {
-	
-	public String log[];
-	public static int logIndex;
+
+	public ArrayList<String> log;
 	
 	public Logger() {
-		log= new String[200];
-		logIndex= 0;
+		log = new ArrayList<String>();
 	}
-	
+
 	public void entry(String s) {
-		log[logIndex]= s;
-		logIndex++;
+		log.add(s);
 	}
-	public void createLogFile(Person p)
-	{
-	try {
+
+	public void createLogFile(Person p) {
 		Calendar cal = new GregorianCalendar();
 		// Get the components of the time
-		int hour24 = cal.get(Calendar.HOUR_OF_DAY);     // 0..23
-		int min = cal.get(Calendar.MINUTE);             // 0..59
-		String hour= Integer.toString(hour24);
-		String minute= Integer.toString(min);
-		String time= hour+":"+minute;
-		
-		  FileOutputStream buf = new FileOutputStream(new File (p.getName()+time+".txt"), true);
-		  ObjectOutputStream oos = new ObjectOutputStream(buf);
-		   
-		  oos.writeObject(log); // Write the String array to a file
-		  oos.close();
-		 
-		  // Read it in??
-		  //FileInputStream fis = new FileInputStream(new File ("StringArray_demo.txt"));
-		  //ObjectInputStream ois = new ObjectInputStream(fis);
-		  //String[] sAI = (String[])ois.readObject();
-		  //System.out.println("sAI=" + Arrays.toString(sAI)); //sAI=[string 1, second string, third Str]
-		 
-		} catch(Exception x) {
-		  x.printStackTrace();
-		  return;
+		int hour24 = cal.get(Calendar.HOUR_OF_DAY); // 0..23
+		int min = cal.get(Calendar.MINUTE); // 0..59
+		String hour = Integer.toString(hour24);
+		String minute = Integer.toString(min);
+		String time = hour + minute;
+		String path = p.getName() + time + ".txt";
+		try {
+			BufferedWriter out = null;
+			File file = new File(path);
+			out = new BufferedWriter(new FileWriter(file));
+			for (String s : log) {
+				out.write(s);
+				out.newLine();
+			}
+			out.close();
+
+		} catch (IOException x) {
+			System.out.println("IO error for " + path + ": " + x.getMessage());
 		}
 	}
 }
